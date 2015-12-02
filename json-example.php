a46b5d08ebd0a4e7d3484cdb68c852b556b03d7e
Adrián Barrio Andrés <statickidz@gmail.com>
2015-12-02 20:48:37 +0100

    Update json-example.php

--- json-example.php
+++ json-example.php
@@ -3,7 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');  
 header('Content-Type: application/json');
-require_once('GoogleTranslate.php');
+require_once('GoogleTranslate.class.php');
 $translator = new GoogleTranslate($_GET['source'],$_GET['target']);
 $translation = $translator->translate($_GET['q']);
 $response = new stdClass();
