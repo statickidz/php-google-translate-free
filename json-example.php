8e18702caeb73810709950f490e479a9d2d1c5c3
Adrián Barrio Andrés <statickidz@gmail.com>
2015-12-20 20:22:26 +0100

    Update json-example.php

--- json-example.php
+++ json-example.php
@@ -4,13 +4,14 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');  
 header('Content-Type: application/json');
 require_once('GoogleTranslate.class.php');
-$translator = new GoogleTranslate($_GET['source'],$_GET['target']);
-$translation = $translator->translate($_GET['q']);
+$translation = GoogleTranslate::translate($_GET['source'],$_GET['target'], $_GET['q']);
+ 
 $response = new stdClass();
 if($translation != '') {
-	$response->status = true;
-	$response->translation = $translation;
+  $response->status = true;
+    $response->translation = $translation;
 } else {
-	$response->status = false;
+    $response->status = false;
 }
+ 
 echo json_encode($response);
